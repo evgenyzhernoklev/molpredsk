9fc260425089a70c47d620e1e4d160061a3ad8fa
Evgeny Zhernoklev <John@Mac-mini-Evgeny-2.local>
2016-11-04 13:32:50 +0300

    custom fields for slider added

--- wp-content/plugins/wp-slick-slider-and-image-carousel/wpsisac-slider-custom-post.php
+++ wp-content/plugins/wp-slick-slider-and-image-carousel/wpsisac-slider-custom-post.php
@@ -11,15 +11,15 @@ function wpsisac_slider_init() {
     'view_item'            => __('View Slick Slider', 'wp-slick-slider-and-image-carousel'),
     'search_items'         => __('Search Slick Slider', 'wp-slick-slider-and-image-carousel'),
     'not_found'            =>  __('No Slick Slider Items found', 'wp-slick-slider-and-image-carousel'),
-    'not_found_in_trash'   => __('No Slick Slider Items found in Trash', 'wp-slick-slider-and-image-carousel'), 
-    '_builtin'             =>  false, 
-    'parent_item_colon'    => '',  	'menu_name'          => _x( 'Slick Slider', 'admin menu', 'wp-slick-slider-and-image-carousel' )
+    'not_found_in_trash'   => __('No Slick Slider Items found in Trash', 'wp-slick-slider-and-image-carousel'),
+    '_builtin'             =>  false,
+    'parent_item_colon'    => '',	'menu_name'          => _x( 'Slick Slider', 'admin menu', 'wp-slick-slider-and-image-carousel' )
   );
   $wpsisac_slider_args = array(
     'labels'              => $wpsisac_slider_labels,
-    'public'              => false,    
+    'public'              => false,
     'show_ui'             => true,
-    'show_in_menu'        => true, 
+    'show_in_menu'        => true,
     'query_var'           => true,
     'rewrite'             => false,
     'capability_type'     => 'post',
@@ -57,7 +57,7 @@ function wpsisac_slider_taxonomies() {
     );
     register_taxonomy( 'wpsisac_slider-category', array( 'slick_slider' ), $args );
 }
-function wpsisac_slider_rewrite_flush() {  
+function wpsisac_slider_rewrite_flush() {
 		wpsisac_slider_init();
     flush_rewrite_rules();
 }
@@ -65,7 +65,7 @@ register_activation_hook( __FILE__, 'wpsisac_slider_rewrite_flush' );
 
 // Manage Category Shortcode wpcolumns
 add_filter("manage_wpsisac_slider-category_custom_column", 'wpsisac_slider_category_columns', 10, 3);
-add_filter("manage_edit-wpsisac_slider-category_columns", 'wpsisac_slider_category_manage_columns'); 
+add_filter("manage_edit-wpsisac_slider-category_columns", 'wpsisac_slider_category_manage_columns');
 function wpsisac_slider_category_manage_columns($theme_columns) {
     $new_columns = array(
             'cb' => '<input type="checkbox" />',
@@ -78,15 +78,15 @@ function wpsisac_slider_category_manage_columns($theme_columns) {
 }
 function wpsisac_slider_category_columns($out, $column_name, $theme_id) {
     $theme = get_term($theme_id, 'wpsisac_slider-category');
-    switch ($column_name) {      
+    switch ($column_name) {
         case 'title':
             echo get_the_title();
         break;
         case 'slider_shortcode':			echo '[slick-slider category="' . $theme_id. '"]<br />';
-			  echo '[slick-carousel-slider category="' . $theme_id. '"]';		  
+			  echo '[slick-carousel-slider category="' . $theme_id. '"]';
         break;
         default:
             break;
     }
-    return $out;   
-}/* Custom meta box for slider link */function wpsisac_add_meta_box() {		add_meta_box('custom-metabox',__( 'Read More Link', 'wp-slick-slider-and-image-carousel' ),'wpsisac_box_callback','slick_slider');}add_action( 'add_meta_boxes', 'wpsisac_add_meta_box' );function wpsisac_box_callback( $post ) {	wp_nonce_field( 'wpsisac_save_meta_box_data', 'wpsisac_meta_box_nonce' );	$value = get_post_meta( $post->ID, 'wpsisac_slide_link', true );	echo '<input type="url" id="wpsisac_slide_link" name="wpsisac_slide_link" value="' . esc_attr( $value ) . '" size="25" /><br />';	echo 'eg. http://www.google.com';}function wpsisac_save_meta_box_data( $post_id ) {	if ( ! isset( $_POST['wpsisac_meta_box_nonce'] ) ) {		return;	}	if ( ! wp_verify_nonce( $_POST['wpsisac_meta_box_nonce'], 'wpsisac_save_meta_box_data' ) ) {		return;	}	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {		return;	}	if ( isset( $_POST['post_type'] ) && 'slick_slider' == $_POST['post_type'] ) {		if ( ! current_user_can( 'edit_page', $post_id ) ) {			return;		}	} else {		if ( ! current_user_can( 'edit_post', $post_id ) ) {			return;		}	}	if ( ! isset( $_POST['wpsisac_slide_link'] ) ) {		return;	}	$link_data = sanitize_text_field( $_POST['wpsisac_slide_link'] );	update_post_meta( $post_id, 'wpsisac_slide_link', $link_data );}add_action( 'save_post', 'wpsisac_save_meta_box_data' );
+    return $out;
+}/* Custom meta box for slider link */function wpsisac_add_meta_box() {		add_meta_box('custom-metabox-slider', 'Настройки слайда', 'wpsisac_box_callback', 'slick_slider');}add_action( 'add_meta_boxes', 'wpsisac_add_meta_box' );function wpsisac_box_callback( $post ) {	wp_nonce_field( 'wpsisac_save_meta_box_data', 'wpsisac_meta_box_nonce' );  $invert = get_post_meta( $post->ID, 'wpsisac_slide_invert', true );  $invert_checked = '';  if ( !empty( $invert ) ) {    $invert_checked = 'checked';  }  echo '<h2>Нужно ли отобразить текст справа</h2>';  echo '<input type="checkbox" id="wpsisac_slide_invert" name="wpsisac_slide_invert" value="' . esc_attr( $invert ) . '" ' . $invert_checked . ' /><br />';	$link = get_post_meta( $post->ID, 'wpsisac_slide_link', true );  echo '<h2>Ссылка на статью</h2>';	echo '<input type="url" id="wpsisac_slide_link" name="wpsisac_slide_link" value="' . esc_attr( $link ) . '" size="25" /><br />';	echo 'Например, http://www.google.com';  $title = get_post_meta( $post->ID, 'wpsisac_slide_title', true );  echo '<br/><h2>Заголовок</h2>';  echo '<input type="text" id="wpsisac_slide_title" name="wpsisac_slide_title" value="' . esc_attr( $title ) . '" size="50" /><br />';  $quote = get_post_meta( $post->ID, 'wpsisac_slide_quote', true );  echo '<h2>Цитата</h2>';  echo '<input type="text" id="wpsisac_slide_quote" name="wpsisac_slide_quote" value="' . esc_attr( $quote ) . '" /><br />';  $projects = get_post_meta( $post->ID, 'wpsisac_slide_projects', true );  echo '<h2>Проекты</h2>';  echo '<input type="text" id="wpsisac_slide_projects" name="wpsisac_slide_projects" value="' . wp_kses( $projects, array( 'br' => array() ) ) . '" /><br />';  $background = get_post_meta( $post->ID, 'wpsisac_slide_background', true );  echo '<h2>Цвет фона</h2>';  echo '<input type="text" id="wpsisac_slide_background" name="wpsisac_slide_background" value="' . esc_attr( $background ) . '" /><br />';  $image_bg = get_post_meta( $post->ID, 'wpsisac_slide_image_bg', true );  $image_bg_checked = '';  if ( !empty( $image_bg ) ) {    $image_bg_checked = 'checked';  }  echo '<h2>Нужна ли картинка в качестве подложки под текст</h2>';  echo '<input type="checkbox" id="wpsisac_slide_image_bg" name="wpsisac_slide_image_bg" value="' . esc_attr( $image_bg ) . '" ' . $image_bg_checked . ' /><br />';}function wpsisac_save_meta_box_data( $post_id ) {	if ( ! isset( $_POST['wpsisac_meta_box_nonce'] ) ) {		return;	}	if ( ! wp_verify_nonce( $_POST['wpsisac_meta_box_nonce'], 'wpsisac_save_meta_box_data' ) ) {		return;	}	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {		return;	}	if ( isset( $_POST['post_type'] ) && 'slick_slider' == $_POST['post_type'] ) {		if ( ! current_user_can( 'edit_page', $post_id ) ) {			return;		}	} else {		if ( ! current_user_can( 'edit_post', $post_id ) ) {			return;		}	}	if ( !isset( $_POST['wpsisac_slide_link'] ) || !isset( $_POST['wpsisac_slide_title'] ) || !isset( $_POST['wpsisac_slide_quote'] ) || !isset( $_POST['wpsisac_slide_projects'] ) || !isset( $_POST['wpsisac_slide_background'] ) ) {		return;	}  $invert = sanitize_text_field( $_POST['wpsisac_slide_invert'] );	$link_data = sanitize_text_field( $_POST['wpsisac_slide_link'] );  $title_data = sanitize_text_field( $_POST['wpsisac_slide_title'] );  $quote_data = sanitize_text_field( $_POST['wpsisac_slide_quote'] );  $projects_data = wp_kses( $_POST['wpsisac_slide_projects'], array( 'br' => array() ) );  $background_data = sanitize_text_field( $_POST['wpsisac_slide_background'] );  $image_bg = sanitize_text_field( $_POST['wpsisac_slide_image_bg'] );  if( isset( $_POST[ 'wpsisac_slide_invert' ] ) ) {      update_post_meta( $post_id, 'wpsisac_slide_invert', 'yes' );  } else {      update_post_meta( $post_id, 'wpsisac_slide_invert', '' );  }	update_post_meta( $post_id, 'wpsisac_slide_link', $link_data );	update_post_meta( $post_id, 'wpsisac_slide_title', $title_data );  update_post_meta( $post_id, 'wpsisac_slide_quote', $quote_data );  update_post_meta( $post_id, 'wpsisac_slide_projects', $projects_data );  update_post_meta( $post_id, 'wpsisac_slide_background', $background_data );  if( isset( $_POST[ 'wpsisac_slide_image_bg' ] ) ) {      update_post_meta( $post_id, 'wpsisac_slide_image_bg', 'yes' );  } else {      update_post_meta( $post_id, 'wpsisac_slide_image_bg', '' );  }}add_action( 'save_post', 'wpsisac_save_meta_box_data' );
